a5a68ed3379233d5d56b7578e37439cc55f7e8e5
GiovanniAM <125399281+GiovanniAM@users.noreply.github.com>
2023-04-06 13:51:13 -0300

    aula 4

---
+++ Lógica_de_Programação/ex004/script/script.js
@@ -0,0 +1,21 @@
+/*
+
+
+//Global functions
+function pl(){
+    document.write("<br>");
+}
+function show(palavras){
+    document.write(palavras);
+    pl();
+}
+
+//Global Variables
+peso1 = 73;
+altura1 = 1.71;
+imc1 = peso1 / (altura1 * altura1);
+
+//main script
+show("O IMC do Naldo é " + imc1); 
+
+*/
