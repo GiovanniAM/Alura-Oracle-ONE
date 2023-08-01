8619fea045bc029b9c043e8ab32c346bcb8208c8
GiovanniAM <125399281+giocodess@users.noreply.github.com>
2023-07-31 23:42:16 -0300

    React aulas 1/2/3

---
+++ Trilha React/01_React_desenvolvendo_com_Javascript/Organograma/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Banner from './components/Banner'
+import CampoTexto from './components/CampoTexto'
+
+function App() {
+  return (
+    <>
+    <div className='App'>
+      <Banner></Banner>
+      <CampoTexto></CampoTexto>
+    </div>
+    </>
+  )
+}
+
+export default App
